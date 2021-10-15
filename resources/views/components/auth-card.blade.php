1b3e054b93263df73eaac7ccc0f003213587e2d8
Johan Tovar <jtovarto@gmail.com>
2021-10-14 21:52:49 -0400

    feat: :sparkles: Add auth UI components

---
+++ resources/views/components/auth-card.blade.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="row mt-lg-n10 mt-md-n11 mt-n10">
+        <div class="mx-auto col-xl-4 col-lg-5 col-md-7">
+            <div class="card z-index-0">
+                <div class="pt-4 text-center card-header">
+                    <h5>{{ $title }}</h5>
+                </div>
+
+                <x-social-auth></x-social-auth>
+
+                <div class="card-body">
+                    {{ $slot }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
