b482f092a99c50a17cdbbb727c7aa318a0e6d9c8
Johan Tovar <jtovarto@gmail.com>
2021-10-16 00:03:24 -0400

    Add date storage for acceptance of terms

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -32,6 +32,7 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'accept_terms' => now(),
         ]);
     }
 }
