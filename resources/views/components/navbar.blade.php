f7fe0eb8c0a0fd7ee62f71e3d60685f312f87b3f
Johan Tovar <jtovarto@gmail.com>
2021-10-22 18:13:17 -0400

    Fix navbar component styles

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,28 +1,28 @@
 <!-- Navbar -->
-<nav {{ $attributes->merge(["class" => "top-0 my-3  navbar navbar-expand-lg position-absolute z-index-3 mx-2"]) }} >
-     <div class="container">
-         {{--<x-logo-brand/>--}}
+<nav {{ $attributes->merge(['class' => 'top-0 my-3  navbar navbar-expand-lg position-absolute z-index-3 mx-2']) }}>
+    <div class="container">
+        {{-- <x-logo-brand/> --}}
         <!-- Logo -->
-        <a class="navbar-brand me-4" href="/">
+        <a class="d-flex navbar-brand me-4 align-items-center" href="/">
             <x-jet-application-mark width="36" />
-            {{ config('app.name', 'Laravel') }}
+            <psan class="mx-2">{{ config('app.name', 'Laravel') }}</psan>
         </a>
-         <button class="shadow-none navbar-toggler ms-2" type="button" data-bs-toggle="collapse"
-             data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
-             aria-label="Toggle navigation">
-             <span class="mt-2 navbar-toggler-icon">
-                 <span class="navbar-toggler-bar bar1"></span>
-                 <span class="navbar-toggler-bar bar2"></span>
-                 <span class="navbar-toggler-bar bar3"></span>
-             </span>
-         </button>
-         <div class="pt-3 pb-2 collapse navbar-collapse w-100 py-lg-0" id="navigation">
-             <div class="mx-auto">
-                 {{ $navlink ?? '' }}
-             </div>
-             <ul class="navbar-nav d-flex">
-                 {{ $buttons ?? '' }}
-             </ul>
-         </div>
-     </div>
- </nav>
+        <button class="shadow-none navbar-toggler ms-2" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="mt-2 navbar-toggler-icon">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+            </span>
+        </button>
+        <div class="pt-3 pb-2 collapse navbar-collapse w-100 py-lg-0" id="navigation">
+            <div class="mx-auto">
+                {{ $navlink ?? '' }}
+            </div>
+            <ul class="navbar-nav d-flex">
+                {{ $buttons ?? '' }}
+            </ul>
+        </div>
+    </div>
+</nav>
