b61ced9e5c504212a9d1983891b34cd82556b749
Alejandro Ramirez <ramalejandro@gmail.com>
2021-10-26 11:39:29 -0500

    feat(Veterinarian form):  ✨ Add veterianarian form route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PetProfileController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -16,21 +14,34 @@ use App\Http\Controllers\PetProfileController;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get("/", function () {
+    return view("home");
+})->name("home");
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/welcome', function () {
-    return view('welcome');
-})->name('welcome');
+Route::middleware(["auth:sanctum", "verified"])
+    ->get("/welcome", function () {
+        return view("welcome");
+    })
+    ->name("welcome");
 
+Route::middleware(["auth:sanctum", "verified"])->group(function () {
+    Route::get("pet/add", [PetProfileController::class, "create"])->name(
+        "pet.create"
+    );
+    Route::get("/pet/{pet}/profile/edit", [
+        PetProfileController::class,
+        "update",
+    ])->name("pet.update");
 
+    Route::get("user/profile", UserProfileController::class)->name(
+        "user.profile"
+    );
 
-Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::get('pet/add', [PetProfileController::class, 'create'])->name('pet.create');
-    Route::get('/pet/{pet}/profile/edit', [PetProfileController::class, 'update'])->name('pet.update');
-
-    Route::get('user/profile', UserProfileController::class)->name('user.profile');
-
-    Route::get('register/profile-information/{step}', WizardProfileController::class)->name('wizard.profile');
+    Route::get(
+        "register/profile-information/{step}",
+        WizardProfileController::class
+    )->name("wizard.profile");
+    Route::get("/pet/{pet}/veterinarian", VeterinarianController::class)->name(
+        "veterinarian"
+    );
 });
