43ad44219c682a0760a61b6e2317fae3f12855c4
Alejandro Ramirez <ramalejandro@gmail.com>
2021-10-28 10:55:24 -0500

    style(User factory): 💄 Comment emergency_conctact_id to avoid its creation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             "lastname" => $this->faker->lastName(),
             "email" => $this->faker->unique()->safeEmail(),
             "email_verified_at" => now(),
-            "emergency_contact_id" => \App\Models\EmergencyContact::factory(),
+            //"emergency_contact_id" => \App\Models\EmergencyContact::factory(),
             "password" =>
                 '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             "remember_token" => Str::random(10),
