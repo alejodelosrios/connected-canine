ed6057956ad278f7e80a30f1d29fc889553ae3de
Alejandro Ramirez <ramalejandro@gmail.com>
2021-10-30 15:28:32 -0500

    feat(Pet details): ✨ Update pet details component
    - Add pet-details-wrap component
    - Update veterinarian view to use pet-details-wrap component

---
+++ resources/views/components/pet-details-wrap.blade.php
@@ -0,0 +1,10 @@
+<div class="py-2">
+    <h3>Pet Details</h3>
+    <div class="d-flex flex-column flex-md-row py-2">
+        <a href="#" class="btn btn-info btn-sm mx-1">Pet Profile</a>
+        <a href="{{ route("veterinarian", $pet)}}" class="btn btn-info btn-sm mx-1">Veterinarian</a>
+        <a href="#" class="btn btn-info btn-sm mx-1">Vaccines</a>
+        <a href="#" class="btn btn-info btn-sm mx-1">Health adn Behavior</a>
+        <a href="#" class="btn btn-info btn-sm mx-1">Medical Details</a>
+    </div>
+</div>
--- resources/views/pet/details.blade.php
+++ resources/views/pet/details.blade.php
@@ -1,12 +1,6 @@
 <x-app-layout>
     <x-card>
-        <h3>Pet Details</h3>
-        <div class="d-flex py-2">
-            <a class="btn btn-info btn-sm mx-1">Pet Profile</a>
-            <a class="btn btn-info btn-sm mx-1">Veterinarian</a>
-            <a class="btn btn-info btn-sm mx-1">Vaccines</a>
-            <a class="btn btn-info btn-sm mx-1">Health adn Behavior</a>
-            <a class="btn btn-info btn-sm mx-1">Medical Details</a>
-        </div>
+        <x-pet-details-wrap :pet="$pet" />
+        {{--Se propone que esta vista sea directamente la de pet profile.--}}
     </x-card>
 </x-app-layout>
