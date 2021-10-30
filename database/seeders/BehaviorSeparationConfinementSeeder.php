76ee9a75f10ec7fd56c55d511d959cd7a7747bff
Johan Tovar <jtovarto@gmail.com>
2021-10-30 10:41:32 -0400

    Add behavior seeders

---
+++ database/seeders/BehaviorSeparationConfinementSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Behavior;
+use Illuminate\Database\Seeder;
+
+class BehaviorSeparationConfinementSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Behavior::factory()->create([
+            'name' => 'Does your dog exhibit any of the following behaviors when separated from you in an unfamiliar location. Please select all the aplly.',
+            'description' => 'behaviors when separated from you in an unfamiliar location',
+            'type' => 'separation_confinement'
+        ]);
+
+        Behavior::factory()->create([
+            'name' => 'Can your pet be crated without vocalizing or otherwise acting distressed?',
+            'description' => 'pet can be crated',
+            'type' => 'separation_confinement'
+        ]);
+    }
+}
---
+++ database/seeders/BehavioralBackgroundSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Behavior;
+use Illuminate\Database\Seeder;
+
+class BehavioralBackgroundSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Behavior::factory()->create([
+            'name' => 'How often does your dog  come when called',
+            'description' => 'come when called',
+            'type' => 'background'
+        ]);
+
+        Behavior::factory()->create([
+            'name' => 'How often does your urinate or deficate indoors',
+            'description' => 'urinate or deficate indoors',
+            'type' => 'background'
+        ]);
+
+        Behavior::factory()->create([
+            'name' => 'Thunder',
+            'description' => 'appear distessed',
+            'type' => 'background'
+        ]);
+
+        Behavior::factory()->create([
+            'name' => 'Vaccums',
+            'description' => 'appear distessed',
+            'type' => 'background'
+        ]);
+    }
+}
