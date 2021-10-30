349954e364a9a894918ee830689353ef7d5540a6
Johan Tovar <jtovarto@gmail.com>
2021-10-30 09:57:04 -0400

    feat: :sparkles: Add behavior entity

---
+++ database/migrations/2021_10_30_135135_create_behaviors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBehaviorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('behaviors', function (Blueprint $table) {
+            $table->id();
+            $table->tinyText('name');
+            $table->tinyText('description');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('behaviors');
+    }
+}
