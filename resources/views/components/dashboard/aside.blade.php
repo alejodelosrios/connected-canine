6219f584c5fafaa1850488c441f454d9cfd7a40c
Alejandro Ramirez <ramalejandro@gmail.com>
2021-10-30 14:38:12 -0500

    style(Dashboard): 💄 Add logout option to sidebar

--- resources/views/components/dashboard/aside.blade.php
+++ resources/views/components/dashboard/aside.blade.php
@@ -43,7 +43,8 @@
                 </x-slot>
             </x-dashboard.nav-link>
 
-             <x-dashboard.nav-link route="user.profile" title="User Profile" :active="request()->is('user/*') || request()->is('user')">
+            <x-dashboard.nav-link route="user.profile" title="User Profile"
+                :active="request()->is('user/*') || request()->is('user')">
                 <x-slot name="icon">
                     <svg width="12px" height="12px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg"
                         xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -69,7 +70,8 @@
                 </x-slot>
             </x-dashboard.nav-link>
 
-            <x-dashboard.nav-link route="pet.index" title="Pets" :active="request()->is('pets/*') || request()->is('pets')">
+            <x-dashboard.nav-link route="pet.index" title="Pets"
+                :active="request()->is('pets/*') || request()->is('pets')">
                 {{-- //TODO: MAKE PETS-INDEX SCREEN --}}
                 <x-slot name="icon">
                     <svg width="12px" height="12px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg"
@@ -151,6 +153,25 @@
                 </x-slot>
             </x-dashboard.nav-link>
 
+            <hr class="mt-4 horizontal dark">
+
+            <li class="nav-item ">
+                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                    <div
+                        class="text-center bg-white shadow icon icon-shape icon-sm border-radius-md me-2 d-flex align-items-center justify-content-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12px" height="12px" viewBox="0 0 24 24">
+                            <path
+                                d="M10 9.408l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7zm6-3c-1.787 0-3.46.474-4.911 1.295l.228.2 1.396 1.221c1.004-.456 2.114-.716 3.287-.716 4.411 0 8 3.589 8 8s-3.589 8-8 8c-1.173 0-2.283-.26-3.288-.715l-1.396 1.221-.228.2c1.452.82 3.125 1.294 4.912 1.294 5.522 0 10-4.477 10-10s-4.478-10-10-10z" />
+                        </svg>
+                    </div>
+                    <span class="nav-link-text ms-1">Log out</span>
+                </a>
+            </li>
+            <form method="POST" id="logout-form" action="{{ route('logout') }}">
+                @csrf
+            </form>
         </ul>
+
     </div>
 </aside>
