c01ada4ae3be6ead353cd5310edad24ae1e25b13
Alejandro Ramirez <ramalejandro@gmail.com>
2021-11-01 09:19:15 -0600

    style(Brand Logo): 💄Place logo in UserMesssage & Booking form views
    - Add background property to navbar component call

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     @section('navbar')
-        <x-navbar class="py-2 mt-4 shadow blur blur-rounded start-0 end-0" />
+        <x-navbar class="py-2 mt-4 shadow blur blur-rounded start-0 end-0"  background="light"/>
     @endsection
 
     @livewire('booking-form')
