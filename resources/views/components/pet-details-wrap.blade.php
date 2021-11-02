06f1e83964ed38e726bdffdddcca6dc37b5ff568
Johan Tovar <jtovarto@gmail.com>
2021-11-01 21:56:14 -0400

    style: :lipstick: Add active class for buttons

--- resources/views/components/pet-details-wrap.blade.php
+++ resources/views/components/pet-details-wrap.blade.php
@@ -1,10 +1,13 @@
+@props([
+    'title'=> 'Non Title'
+])
 <div class="py-2">
-    <h3>Pet Details</h3>
-    <div class="d-flex flex-column flex-md-row py-2">
-        <a href="{{ route("pet.update", $pet)}}" class="btn btn-primary btn-sm mx-1">Pet Profile</a>
-        <a href="{{ route("veterinarian", $pet)}}" class="btn btn-primary btn-sm mx-1">Veterinarian</a>
-        <a href="#" class="btn btn-primary btn-sm mx-1">Vaccines</a>
-        <a href="#" class="btn btn-primary btn-sm mx-1">Health adn Behavior</a>
-        <a href="#" class="btn btn-primary btn-sm mx-1">Medical Details</a>
+    <h4>{{ $title }}</h4>
+    <div class="py-2 d-flex flex-column flex-md-row">
+        <a href="{{ route("pet.update", $pet)}}" class="mx-1 btn btn-primary btn-sm @if(request()->is('pets/*/profile')) active @endif">Pet Profile</a>
+        <a href="{{ route("veterinarian", $pet)}}" class="mx-1 btn btn-primary btn-sm @if(request()->is('pet/*/veterinarian')) active @endif">Veterinarian</a>
+        <a href="#" class="mx-1 btn btn-primary btn-sm @if(request()->is('#/*') || request()->is('#')) active @endif">Vaccines</a>
+        <a href="#" class="mx-1 btn btn-primary btn-sm @if(request()->is('#/*') || request()->is('#')) active @endif">Health adn Behavior</a>
+        <a href="#" class="mx-1 btn btn-primary btn-sm @if(request()->is('v#/*') || request()->is('#')) active @endif">Medical Details</a>
     </div>
 </div>
