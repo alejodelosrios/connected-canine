1284f7840820ae82a8ed170e4c88c09727a99e3f
Alejandro Ramirez <ramalejandro@gmail.com>
2021-11-08 08:47:46 -0600

    style(Pet details): 💄Fix tablets view in Pet Details Wrap component
    - Spelling errors are corrected

--- resources/views/components/pet-details-wrap.blade.php
+++ resources/views/components/pet-details-wrap.blade.php
@@ -3,11 +3,11 @@
 ])
 <div class="py-2">
     <h4>{{ $title }}</h4>
-    <div class="py-2 d-flex flex-column flex-md-row">
+    <div class="py-2 d-flex flex-column flex-md-row flex-md-wrap">
         <a href="{{ route("pet.update", $pet)}}" class="mx-1 btn btn-primary btn-sm @if(request()->is('pets/*/profile')) active @endif">Pet Profile</a>
         <a href="{{ route("veterinarian", $pet)}}" class="mx-1 btn btn-primary btn-sm @if(request()->is('pet/*/veterinarian')) active @endif">Veterinarian</a>
         <a href="{{ route('vaccines',$pet) }}" class="mx-1 btn btn-primary btn-sm @if(request()->is('pets/*/vaccines')) active @endif">Vaccines</a>
-        <a href="{{ route('pet.behavior.backgroung',$pet) }}" class="mx-1 btn btn-primary btn-sm">Health adn Behavior</a>
+        <a href="{{ route('pet.behavior.backgroung',$pet) }}" class="mx-1 btn btn-primary btn-sm">Health & Behavior</a>
         <a href="{{ route("pet.medications", $pet)}}" class="mx-1 btn btn-primary btn-sm @if(request()->is('pets/*/medications') || request()->is('/pets/*/medications/*/update')) active @endif">Medications</a>
     </div>
 </div>
