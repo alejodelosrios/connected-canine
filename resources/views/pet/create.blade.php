3faa1da13ee0e08f4975339654da04dba215eda6
Johan Tovar <jtovarto@gmail.com>
2021-11-08 20:12:27 -0400

    fix: Update add pet screen redirection

--- resources/views/pet/create.blade.php
+++ resources/views/pet/create.blade.php
@@ -6,12 +6,12 @@
             <x-slot name="footer">
                 <div class="d-flex align-items-baseline justify-content-end">
                     <div>
-                        <a href="{{ route('pet.index') }}" class="btn btn-secondary mx-2">
+                        <a href="{{ route('pet.index') }}" class="mx-2 btn btn-secondary">
                             {{ __('Cancel') }}
                         </a>
                     </div>
                     @livewire('save-button', [
-                    'redirect_route_name' => 'pet.index'
+                    'redirect_route_name' => route('pet.index')
                     ])
                 </div>
             </x-slot>
