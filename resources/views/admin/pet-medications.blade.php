087c8ed8b5eaa1cde7aebf6a0b16926991a4709e
Alejandro Ramirez <ramalejandro@gmail.com>
2021-11-09 19:10:47 -0600

    feat(Admin): 💄 Add routes, controller & views
    - Add Medications, veterinarian, profile, and vaccines admin views

---
+++ resources/views/admin/pet-medications.blade.php
@@ -0,0 +1,66 @@
+<x-app-layout>
+    <x-card>
+        <x-pet-details-wrap :pet="$pet" title="Pet Medications" />
+
+        <div class="table-responsive">
+            <table class="table align-items-center mb-0">
+                <thead>
+                    <tr>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Id</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Name</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Frequency
+                        </th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                            Time block</th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                            Dosage</th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                            Instructions</th>
+                        <th class="text-end text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                            Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($pet->medications as $medication)
+                        <tr>
+                            <td class="text-center">
+                                <p class="text-xs font-weight-bold mb-0 text-capitalize">{{ $medication->id }}</p>
+                            </td>
+                            <td class="align-middle text-sm">
+                                <p class="text-xs font-weight-bold mb-0 text-capitalize">{{ $medication->name }}</p>
+                            </td>
+                            <td class="align-middle text-center text-sm">
+                                <span
+                                    class=" text-xs font-weight-bold text-capitalize">{{ $medication->frequency }}</span>
+                            </td>
+                            <td class="align-middle text-center text-sm">
+                                <span
+                                    class=" text-xs font-weight-bold text-capitalize">{{ $medication->time_block }}</span>
+                            </td>
+                            <td class="align-middle text-center text-sm">
+                                <span
+                                    class=" text-xs font-weight-bold text-capitalize">{{ $medication->dosage }}</span>
+                            </td>
+                            <td class="align-middle text-center text-sm">
+                                <span
+                                    class=" text-xs font-weight-bold text-capitalize">{{ $medication->instructions }}</span>
+                            </td>
+                            <td class="text-end align-middle">
+                                <a href="{{ route('admin.pet-medication-details', [$pet, $medication]) }}"
+                                    class="text-secondary font-weight-bold text-xs pe-3" data-toggle="tooltip"
+                                    data-original-title="Edit medication" data-bs-toggle="tooltip" data-bs-placement="top"
+                                    title="Edit medication" data-container="body" data-animation="true">
+                                    View details
+                                </a>
+                            </td>
+                        </tr>
+                    @empty
+                        <td colspan="5" class="p-4">
+                            {{ __('There are not medications for this pet') }}
+                        </td>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </x-card>
+</x-app-layout>
---
+++ resources/views/admin/pet-profile.blade.php
@@ -0,0 +1,60 @@
+<x-app-layout>
+    <x-card>
+        <x-pet-details-wrap :pet="$pet" title="Pet Profile" />
+        <div>
+            <div class="mb-3">
+                <x-jet-label for="photo" value="{{ __('Photo') }}" />
+
+                {{-- Current Profile Photo --}}
+
+                <div class="mt-2">
+                    @if ($pet->profile_photo_path)
+                        <img src="{{ $pet->profile_photo_path }}" class="rounded-circle" height="80px" width="80px">
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                {{-- Name --}}
+                <div class="mb-3">
+                    <x-jet-label for="name" value="{{ __('Name') }}" />
+                    <p>
+                        {{ $pet->name }}
+                    </p>
+                </div>
+
+                {{-- Birthday --}}
+                <div class="mb-3 col-12 col-md-6">
+                    <x-jet-label for="birthday" value="{{ __('Birthday') }}" />
+                    <p>
+                        {{ $pet->birthday }}
+                    </p>
+                </div>
+
+                {{-- Sex --}}
+                <div class="mb-3 col-12 col-md-6">
+                    <x-jet-label for="sex" value="{{ __('Sex') }}" />
+                    <p>
+                        {{ $pet->sex }}
+                    </p>
+                </div>
+
+                {{-- Weight --}}
+                <div class="mb-3 col-12 col-md-6">
+                    <x-jet-label for="weight" value="{{ __('Weight') }}" />
+                    <p>
+                        {{ $pet->weight }}
+                    </p>
+                </div>
+
+                {{-- Color --}}
+                <div class="mb-3 col-12 col-md-6">
+                    <x-jet-label for="color" value="{{ __('Color') }}" />
+                    <p>
+                        {{ $pet->color }}
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </x-card>
+</x-app-layout>
