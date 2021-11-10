c13865ab23aa0feba82f53990e8c7e6026ee10fc
Alejandro Ramirez <ramalejandro@gmail.com>
2021-11-09 20:56:43 -0600

    style(Footer):  💄 Add The Connected Canine link on admin layout footer

--- resources/views/components/dashboard/footer.blade.php
+++ resources/views/components/dashboard/footer.blade.php
@@ -15,7 +15,7 @@
             <div class="col-lg-6">
                 <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted"
+                        <a href="https://connectedcanine.com" class="nav-link pe-0 text-muted"
                             target="_blank">Agreement</a>
                     </li>
                 </ul>
