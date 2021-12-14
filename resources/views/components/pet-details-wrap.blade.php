2a9433d9f0246b3d8c4ff4b72b926201959b23ce
Alejandro Ramirez <ramalejandro@gmail.com>
2021-12-13 19:53:02 -0600

    style(Veterinarian): 💄 Customer requests
    - Tab title changed to "Vet & Medical"

--- resources/views/components/pet-details-wrap.blade.php
+++ resources/views/components/pet-details-wrap.blade.php
@@ -5,7 +5,7 @@
         <a href="{{ route('admin.pet-profile', $pet) }}" class="mx-1 btn btn-primary btn-sm @if (request()->is('admin/pets/*/profile')) active @endif">Pet
             Profile</a>
         <a href="{{ route('admin.pet-veterinarian', $pet) }}"
-            class="mx-1 btn btn-primary btn-sm @if (request()->is('admin/pets/*/veterinarian')) active @endif">Veterinarian</a>
+            class="mx-1 btn btn-primary btn-sm @if (request()->is('admin/pets/*/veterinarian')) active @endif">Vet & Medical</a>
         <a href="{{ route('admin.pet-vaccines', $pet) }}"
             class="mx-1 btn btn-primary btn-sm @if (request()->is('admin/pets/*/vaccines')) active @endif">Vaccines</a>
         <a href="{{ route('admin.behavior.background', $pet) }}" class="mx-1 btn btn-primary btn-sm">Behavior</a>
@@ -15,7 +15,7 @@
         <a href="{{ route('pet.update', $pet) }}" class="mx-1 btn btn-primary btn-sm @if (request()->is('pets/*/profile')) active @endif">Pet
             Profile</a>
         <a href="{{ route('veterinarian', $pet) }}"
-            class="mx-1 btn btn-primary btn-sm @if (request()->is('pet/*/veterinarian')) active @endif">Veterinarian</a>
+            class="mx-1 btn btn-primary btn-sm @if (request()->is('pet/*/veterinarian')) active @endif">Vet & Medical</a>
         <a href="{{ route('vaccines', $pet) }}"
             class="mx-1 btn btn-primary btn-sm @if (request()->is('pets/*/vaccines')) active @endif">Vaccines</a>
         <a href="{{ route('pet.behavior.backgroung', $pet) }}" class="mx-1 btn btn-primary btn-sm">Behavior</a>
