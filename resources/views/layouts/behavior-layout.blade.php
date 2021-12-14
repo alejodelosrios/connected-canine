4492aef0e28102142e13d6b4fc7c7272bdf80bea
Johan Tovar <jtovarto@gmail.com>
2021-12-13 22:50:17 -0400

    Chnage doggy daycare history tab name

--- resources/views/layouts/behavior-layout.blade.php
+++ resources/views/layouts/behavior-layout.blade.php
@@ -7,7 +7,7 @@
                     class="mx-1 btn btn-primary btn-sm @if (request()->is('pets/*/behaviors/background')) active @endif">Behavior Background</a>
 
                 <a href="{{ route('pet.boarding-history', $pet) }}"
-                    class="mx-1 btn btn-primary btn-sm @if (request()->is('pets/*/behaviors/boarding-history')) active @endif">Doggie Daycare History</a>
+                    class="mx-1 btn btn-primary btn-sm @if (request()->is('pets/*/behaviors/boarding-history')) active @endif">Doggy Daycare History</a>
 
                 <a href="{{ route('pet.behavior.separation-confinement', $pet) }}"
                     class="mx-1 btn btn-primary btn-sm @if (request()->is('pets/*/behaviors/separation-confinement')) active @endif">Separation and Confinement</a>
