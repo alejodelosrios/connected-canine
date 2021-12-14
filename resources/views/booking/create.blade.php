a2a04ab8cf2884ccc97a34b940eee120910913a3
Alejandro Ramirez <ramalejandro@gmail.com>
2021-12-13 20:52:05 -0600

    style(Bookings): 💄 Customer requests
    - New NavLink for bookings in Admin Panel added
    - Bookings icons added
    - Redirect after booking save changed to welcome screen

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -1,7 +1,7 @@
-<x-guest-layout>
-    @section('navbar')
-        <x-navbar class="py-2 mt-4 shadow blur blur-rounded start-0 end-0"  background="light"/>
-    @endsection
+<x-app-layout>
+    {{--@section('navbar')--}}
+        {{--<x-navbar class="py-2 mt-4 shadow blur blur-rounded start-0 end-0"  background="light"/>--}}
+    {{--@endsection--}}
 
     @livewire('booking-form')
-</x-guest-layout>>
+</x-app-layout>>
