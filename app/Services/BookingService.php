e2eac4a042ada6dd9fa4a48a3917776dcf370a04
Alejandro Ramirez <ramalejandro@gmail.com>
2022-01-15 16:05:02 -0600

    style(Booking): 💄 Booking error updated
    - Delete "or pending approval" on booking error

--- app/Services/BookingService.php
+++ app/Services/BookingService.php
@@ -9,19 +9,22 @@ use Illuminate\Support\Facades\Validator;
 
 final class BookingService implements UpdaterContract
 {
-
     //TODO: CHECK FOR CUSTOM MESSAGGE DISLAY
     protected $custom_message = [
-        'pet_id.required' => 'Select Your Pet',
-        'pet_id.exists' => 'The chosen pet does not appear in our records',
-        'pet_id.unique' => 'The chosen pet already has an approved reservation or pending approval',
-        'date.required' => 'Date is required',
-        'date.after' => 'The date must be after today',
+        "pet_id.required" => "Select Your Pet",
+        "pet_id.exists" => "The chosen pet does not appear in our records",
+        "pet_id.unique" => "The chosen pet already has an approved reservation",
+        "date.required" => "Date is required",
+        "date.after" => "The date must be after today",
     ];
 
     public function save(array $input)
     {
-        $validated = Validator::make($input, $this->rules($input), $this->custom_message)->validateWithBag('save');
+        $validated = Validator::make(
+            $input,
+            $this->rules($input),
+            $this->custom_message
+        )->validateWithBag("save");
 
         \App\Models\Booking::create($validated);
     }
@@ -29,14 +32,17 @@ final class BookingService implements UpdaterContract
     private function rules($input)
     {
         return [
-            'pet_id' => [
-                'required', 'exists:pets,id',
-                Rule::unique('bookings')->where(function ($query) use ($input) {
-                    return $query->where('pet_id', $input['pet_id'])->where('date', '>=', Carbon::today())
-                        ->where('status', '<>', \App\Models\Booking::CANCELLED);
-                })
+            "pet_id" => [
+                "required",
+                "exists:pets,id",
+                Rule::unique("bookings")->where(function ($query) use ($input) {
+                    return $query
+                        ->where("pet_id", $input["pet_id"])
+                        ->where("date", ">=", Carbon::today())
+                        ->where("status", "<>", \App\Models\Booking::CANCELLED);
+                }),
             ],
-            'date' => ['required', 'after:today']
+            "date" => ["required", "after:today"],
         ];
     }
 }
