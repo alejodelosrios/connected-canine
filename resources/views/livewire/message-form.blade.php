7ee42144bc45fa65403b40acfe0b7994abf39587
Alejandro Ramirez <ramalejandro@gmail.com>
2022-01-15 09:04:18 -0600

    style(Contact us): 💄 Customer requests
    - Phone added

--- resources/views/livewire/message-form.blade.php
+++ resources/views/livewire/message-form.blade.php
@@ -9,7 +9,8 @@
             <h3>Reach Out, We Don’t Bite</h3>
 
             <div class=" p-2">
-                <h6>Email us with any questions or comments. We’re here to help.</h6>
+                <h6>Email or call us with any questions or comments. We’re here to help. </h6>
+                <h6><strong>Phone: </strong>(703) 260-9229</h6>
                 <div class="mb-3 col-12 ">
                     <textarea wire:model.defer="message" name="message" id="message" class="form-control  {{ $errors->has('message') ? 'is-invalid' : '' }}"></textarea>
                     <x-jet-input-error for="message" />
