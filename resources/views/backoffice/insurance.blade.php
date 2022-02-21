205eb3181ace0f8c95818eb666b87fe0c44a37c4
Alejandro Ramirez <ramalejandro@gmail.com>
2022-02-20 18:26:05 -0600

    feat(Final client requests): Save button deleted from insurance view ✨

--- resources/views/backoffice/insurance.blade.php
+++ resources/views/backoffice/insurance.blade.php
@@ -3,13 +3,13 @@
         <x-card>
             <x-user-details-wrap :user="$user" title="Insurance" />
             @livewire('insurance-form',['user'=>$user])
-            <x-slot name="footer">
-                @if ($user->insurance === null || $user->insurance->proof === null)
-                    <div class="d-flex align-items-baseline justify-content-end">
-                        @livewire('save-button', ['redirect_route_name' => route('insurance',$user)])
-                    </div>
-                @endif
-            </x-slot>
+            {{--<x-slot name="footer">--}}
+                {{--@if ($user->insurance === null || $user->insurance->proof === null)--}}
+                    {{--<div class="d-flex align-items-baseline justify-content-end">--}}
+                        {{--@livewire('save-button', ['redirect_route_name' => route('insurance',$user)])--}}
+                    {{--</div>--}}
+                {{--@endif--}}
+            {{--</x-slot>--}}
         </x-card>
     </div>
 </x-app-layout>
