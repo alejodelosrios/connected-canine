963ac9b6688fdf71acffa82addf9dd30c89261a4
Alejandro Ramirez <ramalejandro@gmail.com>
2022-02-25 22:23:39 -0600

    fix(Vaccines proof): See document protected 🐛
    - Only showed when pet has a vaccine proof attached

--- resources/views/livewire/vaccine-form.blade.php
+++ resources/views/livewire/vaccine-form.blade.php
@@ -20,7 +20,9 @@
         @isset($state['proof'])
             <div class="gap-2 d-flex align-items-center">
                 <div class="p-2 border rounded flex-fill">
-                    <a href="{{ route('vaccine-proof', $pet) }}" target="_blank">See document</a>
+                    @if ($pet->vaccines)
+                        <a href="{{ route('vaccine-proof', $pet) }}" target="_blank">See document</a>
+                    @endif
                 </div>
                 <div>
                     <x-jet-button class="mt-3" type="button" wire:click="removeProof">Remove file</x-jet-button>
